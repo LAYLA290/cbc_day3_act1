4ae07fd56f31a560bc0b6be2331fa28612b16051
WOW <136232442+LAYLA290@users.noreply.github.com>
2025-05-06 22:11:47 -1200

    Add files via upload

---
+++ Bootcamp_act1.py
@@ -0,0 +1,10 @@
+phrase = "coding bootcamp2025"
+
+print("Is Alpha: ", phrase.isalpha())
+print("Is Digit: ", phrase.isdigit())
+print("Capitalize First Letter: ", phrase.capitalize())
+print("Lowercase: ", phrase.lower())
+print("Is Upper: ", phrase.isupper())
+print(phrase.capitalize().center(50))
+print(phrase.capitalize().ljust(50))
+print(phrase.capitalize().rjust(50))
